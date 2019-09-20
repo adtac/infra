443a176db1596ecb995cd67f8a683fadbdeb75d9
Adhityaa Chandrasekar <adtac@google.com>
2019-09-20 14:15:29 -0700

    zsh: use regular prompt for google machines

--- global/etc/zsh/.zsh/prompt.zsh
+++ global/etc/zsh/.zsh/prompt.zsh
@@ -1,7 +1,11 @@
 case "$HOST" in
-  arch-*) host_bg=15;  host_fg=119;;
-  *)      host_bg=119; host_fg=112;;
+  arch-*)
+    host_bg=15;  host_fg=119;;
+  *.google.com)
+    host_bg=15;  host_fg=119;;
+  *)
+    khost_bg=119; host_fg=112;;
 esac
 
-PS1=" %K{$host_bg}%B%F{$host_fg} @$HOST %f%b%k %B%F{1}"'$(pwd | sed "s:${HOME}:~:")'"%f%b %B%F{2}\$%f%b "
+PS1="%K{$host_bg}%B%F{$host_fg} @$HOST %f%b%k %B%F{1}"'$(pwd | sed "s:${HOME}:~:")'"%f%b %B%F{2}\$%f%b "
 RPROMPT=""
