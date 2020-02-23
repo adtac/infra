c8af25f03c0577788df3db30547ad28807a2bb77
Adhityaa Chandrasekar <adtac@adtac.in>
2020-02-23 16:54:54 -0500

    zsh: remove unused hosts

--- global/etc/zsh/.zsh/prompt.zsh
+++ global/etc/zsh/.zsh/prompt.zsh
@@ -1,12 +1,6 @@
 case "$HOST" in
-  arch-*)
-    host_bg=15;  host_fg=119;;
-  *.google.com)
-    host_bg=15;  host_fg=119;;
-  3.adtac.in)
-    host_bg=15;  host_fg=119;;
-  *)
-    khost_bg=119; host_fg=112;;
+  3.adtac.in) host_bg=15;  host_fg=119;;
+  *)          host_bg=119; host_fg=112;;
 esac
 
 PS1="%K{$host_bg}%B%F{$host_fg} @$HOST %f%b%k %B%F{1}"'$(pwd | sed "s:${HOME}:~:")'"%f%b %B%F{2}\$%f%b "
