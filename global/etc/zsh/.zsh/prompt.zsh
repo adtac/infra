4ba3a5cd7813042bdd26bc33b19e313b304d0393
Adhityaa Chandrasekar <adtac@adtac.in>
2020-06-15 16:56:21 -0400

    zsh: add corp machines to prompt switch

--- global/etc/zsh/.zsh/prompt.zsh
+++ global/etc/zsh/.zsh/prompt.zsh
@@ -1,6 +1,8 @@
 case "$HOST" in
-  3.adtac.in) host_bg=15;  host_fg=119;;
-  *)          host_bg=119; host_fg=112;;
+  adtac1.roam.corp.google.com|3.adtac.in)
+    host_bg=15;  host_fg=119;;
+  *)
+    host_bg=119; host_fg=112;;
 esac
 
 PS1="%K{$host_bg}%B%F{$host_fg} @$HOST %f%b%k %B%F{1}"'$(pwd | sed "s:${HOME}:~:")'"%f%b %B%F{2}\$%f%b "
